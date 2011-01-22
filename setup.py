8a06cef86232d179a66d5d0ecb1ad1bebd3aa868
Maciek Borzecki <maciek.borzecki@gmail.com>
2011-01-22 11:15:04 +0100

    adding setup script

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup (
+    name = 'q3alert',
+    description = 'Q3 server monitoring applet',
+    long_description = 'An applet to monitor Q3 server and notify the user about games in progress',
+    version = '0.1',
+    author = 'Maciek Borzecki',
+    author_email = 'maciek.borzecki@gmail.com',
+    license = 'GPL',
+    scripts = ['q3alert'],
+    data_files = [('share/q3alert', ['settings-dialog.ui']),
+                  ('share/pixmaps/q3alert', ['bw.svg', 'bw-polling.svg', 'colored.svg'])]
+    )
